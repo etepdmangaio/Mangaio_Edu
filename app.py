1f2cf2e254cf2a7ff91b168e01f435d0968cab0c
Guilhermeazevedo1 <guilhermeazevedo_2603@hotmail.com>
2025-04-25 13:58:25 -0300

    criação das sessões deflagração, repressão e consequências

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 from sidebar_components.matematica import sidebar_matematica
 from sidebar_components.geografia import sidebar_geografia
 from sidebar_components.historia import sidebar_historia2
+from sidebar_components.historia import sidebar_historia_deflagracao_revolta_1824
+from sidebar_components.historia import sidebar_historia_repressao_imperial
+from sidebar_components.historia import sidebar_historia_consequencias
 # from sidebar_components.Informacoes import sidebar_informacoes
 
 
@@ -13,12 +16,20 @@ with st.sidebar.expander("História", expanded=False):
     st.markdown("### Subtemas")
     subtema_escolhido = st.radio("Escolha um subtema", [
         "Crise do Primeiro Reinado", "Insatisfação do Nordeste", "Influências Liberais e Republicanas",
-        "Deflagração da Revolta (1824)"], key="subtema")
+        "Deflagração da Revolta (1824)", "Repressão Imperial", "Consequências"], key="subtema")
 st.write("Subtema selecionado:", subtema_escolhido)
 
 if subtema_escolhido == "Crise do Primeiro Reinado":
     sidebar_historia2()
 
+if subtema_escolhido == "Deflagração da Revolta (1824)":
+    sidebar_historia_deflagracao_revolta_1824()
+
+if subtema_escolhido == "Repressão Imperial":
+    sidebar_historia_repressao_imperial()
+
+if subtema_escolhido == "Consequências":
+    sidebar_historia_consequencias()
 
 sidebar_geografia()
 
